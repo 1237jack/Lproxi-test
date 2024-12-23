fe3942044e7d497d36105c07be64cbd6065b0097
1237jack <121198285+1237jack@users.noreply.github.com>
2024-12-22 20:33:32 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 document.querySelector('.button-confirm').addEventListener('click', function(event) {
   event.preventDefault();
 
-  const username = document.querySelector('input[name="username"]').value;
-  const password = document.querySelector('input[name="password"]').value;
+  const set1dset1 = document.querySelector('input[name="set1dset1"]').value;
+  const set2dset2 = document.querySelector('input[name="set2dset2"]').value;
 
   // Check if the username and password match
-  if (username === 'Admin' && password === 'Rockclimbing2024') {
+  if (set1dset1 === 'Admin' && set2dset2 === 'Rockclimbing2024') {
     sessionStorage.setItem('loggedIn', 'true'); // Store the login state
     window.location.href = 'dashboard.html'; // Redirect to protected page
   } else {
